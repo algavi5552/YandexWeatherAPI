8e277a6434f609d1b7271b7770cd8e79be3b38ac
algavi5552 <56834981+algavi5552@users.noreply.github.com>
2020-07-20 00:28:31 +0300

    Add files via upload

---
+++ YandexWeatherAPI-master/bin/Debug/dbo.Table.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Temp] NVARCHAR(50) NULL, 
+    [Feels_like] NVARCHAR(50) NULL, 
+    [Wind_speed] NVARCHAR(50) NULL, 
+    [Pressure_mm] NVARCHAR(50) NULL, 
+    [Humidity] NVARCHAR(50) NULL, 
+    [Season] NVARCHAR(50) NULL, 
+    [DateTime] NVARCHAR(50) NULL
+)
